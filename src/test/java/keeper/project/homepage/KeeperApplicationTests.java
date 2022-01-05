e8f43c15405e705ece8502c7303e72a643faa411
gram_jhm <gusah009@naver.com>
2022-01-05 16:41:02 +0900

    chore: 초기설정
    - 폴더 구조 설정
    - gitignore 설정
    - build.gradle 설정
    - spring rest docs 설정
    
    issue : #1

---
+++ src/test/java/keeper/project/homepage/KeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package keeper.project.homepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeeperApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
