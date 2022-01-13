482a1318bbe8bce1dd7a6915f1398fd005257c97
핸모정 <gusah009@naver.com>
2022-01-13 11:04:36 +0900

    feat : MessageConfig 지역설정 추가

--- src/main/java/keeper/project/homepage/config/message/MessageConfig.java
+++ src/main/java/keeper/project/homepage/config/message/MessageConfig.java
@@ -1,17 +1,38 @@
 package keeper.project.homepage.config.message;
 
 
-import keeper.project.homepage.service.ResponseService;
+import java.util.Locale;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
-public class MessageConfig {
+public class MessageConfig implements WebMvcConfigurer {
 
-  ResponseService responseService;
+  @Bean // 세션에 지역설정. default는 KOREAN = 'ko'
+  public LocaleResolver localeResolver() {
+    SessionLocaleResolver slr = new SessionLocaleResolver();
+    slr.setDefaultLocale(Locale.KOREAN);
+    return slr;
+  }
 
+  @Bean // 지역설정을 변경하는 인터셉터. 요청시 파라미터에 lang 정보를 지정하면 언어가 변경됨.
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+    lci.setParamName("lang");
+    return lci;
+  }
+
+  @Override // 인터셉터를 시스템 레지스트리에 등록
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(localeChangeInterceptor());
+  }
 
   @Bean
   public MessageSource messageSource() {
