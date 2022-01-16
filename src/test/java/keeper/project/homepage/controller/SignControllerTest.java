f03cfa61810a5e29fa3576356da635bc26d0d028
최우창 <clllickme@naver.com>
2022-01-16 21:26:21 +0900

    test: 중복 검사 테스트
    변경된 Member Entity에 맞게 회원가입 테스트 수정, 로그인 아이디, 이메일, 학번 중복 검사 테스트 생성
    
    issue: #5, #17

--- src/test/java/keeper/project/homepage/controller/SignControllerTest.java
+++ src/test/java/keeper/project/homepage/controller/SignControllerTest.java
@@ -1,14 +1,17 @@
 package keeper.project.homepage.controller;
 
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Collections;
+import java.util.Date;
 import keeper.project.homepage.entity.MemberEntity;
 import keeper.project.homepage.repository.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -54,10 +57,7 @@ public class SignControllerTest {
   final private String realName = "JeongHyeonMo";
   final private String nickName = "HyeonMoJeong";
   final private String birthday = "19980101";
-  final private String homepage = "www.naver.com";
-  final private String blog = "www.google.com";
   final private String studentId = "201724579";
-  final private String phoneNumber = "0100100100";
 
   @BeforeEach
   public void setUp() throws Exception {
@@ -67,14 +67,18 @@ public class SignControllerTest {
         .alwaysDo(print())
         .build();
 
+    SimpleDateFormat stringToDate = new SimpleDateFormat("yyyymmdd");
+    Date birthdayDate = stringToDate.parse(birthday);
+
     memberRepository.save(
         MemberEntity.builder()
             .loginId(loginId)
+            .emailAddress(emailAddress)
             .password(passwordEncoder.encode(password))
             .realName(realName)
-            .emailAddress(emailAddress)
+            .nickName(nickName)
+            .birthday(birthdayDate)
             .studentId(studentId)
-            .phoneNumber(phoneNumber)
             .roles(Collections.singletonList("ROLE_USER"))
             .build());
   }
@@ -109,29 +113,16 @@ public class SignControllerTest {
   }
 
   @Test
-  @DisplayName("아이디 중복검사")
-  public void checkLoginIdDuplication() throws Exception {
-    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-    params.add("loginId", loginId);
-    mockMvc.perform(post("/v1/exist").params(params))
-        .andExpect(status().isOk())
-        .andDo(print());
-  }
-
-  @Test
-  @DisplayName("회원가입")
+  @DisplayName("회원가입 성공 시")
   public void signUp() throws Exception {
     long epochTime = LocalDateTime.now().atZone(ZoneId.systemDefault()).toEpochSecond();
     MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
     params.add("loginId", loginId + epochTime);
     params.add("emailAddress", emailAddress + epochTime);
     params.add("password", password);
-    params.add("realName", realName + epochTime);
+    params.add("realName", realName);
     params.add("nickName", nickName);
     params.add("birthday", birthday);
-    params.add("homepage", homepage);
-    params.add("blog", blog);
-    params.add("phoneNumber", phoneNumber + epochTime);
     params.add("studentId", studentId + epochTime);
     mockMvc.perform(post("/v1/signup").params(params))
         .andDo(print())
@@ -139,5 +130,35 @@ public class SignControllerTest {
         .andExpect(jsonPath("$.success").value(true))
         .andExpect(jsonPath("$.code").value(0))
         .andExpect(jsonPath("$.msg").exists());
+  }
+
+  @Test
+  @DisplayName("아이디 중복 검사 - 중복 존재 : data=true / 존재 X : data=false")
+  public void checkLoginIdDuplication() throws Exception {
+    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+    params.add("loginId", loginId);
+    mockMvc.perform(get("/v1/signup/checkloginidduplication").params(params))
+        .andExpect(status().isOk())
+        .andDo(print());
+  }
+
+  @Test
+  @DisplayName("이메일 중복 검사 - 중복 존재 : data=true / 존재 X : data=false")
+  public void checkEmailAddressDuplication() throws Exception {
+    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+    params.add("emailAddress", emailAddress);
+    mockMvc.perform(get("/v1/signup/checkemailaddressduplication").params(params))
+        .andExpect(status().isOk())
+        .andDo(print());
+  }
+
+  @Test
+  @DisplayName("학번 중복 검사 - 중복 존재 : data=true / 존재 X : data=false")
+  public void checkStudentIdDuplication() throws Exception {
+    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+    params.add("studentId", studentId);
+    mockMvc.perform(get("/v1/signup/checkstudentidduplication").params(params))
+        .andExpect(status().isOk())
+        .andDo(print());
   }
 }
