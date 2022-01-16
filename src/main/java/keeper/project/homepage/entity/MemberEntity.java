1d18953ec8196ebb11477bea93bfa3726aa92691
최우창 <clllickme@naver.com>
2022-01-16 21:21:33 +0900

    feat: Member Entity 수정
    DB 대조표와 비교하여 Member Entity 수정, default Time 주입 관련 코드 삭제
    
    issue: #5, #17

--- src/main/java/keeper/project/homepage/entity/MemberEntity.java
+++ src/main/java/keeper/project/homepage/entity/MemberEntity.java
@@ -1,9 +1,9 @@
 package keeper.project.homepage.entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.stream.Collectors;
 import javax.persistence.Column;
@@ -29,14 +29,14 @@ import org.springframework.security.core.userdetails.UserDetails;
 @NoArgsConstructor          // 인자없는 생성자를 자동으로 생성합니다.
 @AllArgsConstructor         // 인자를 모두 갖춘 생성자를 자동으로 생성합니다.
 @Table(name = "member")     // 'member' 테이블과 매핑됨을 명시
-public class MemberEntity extends BaseTimeEntity implements UserDetails {
+public class MemberEntity implements UserDetails {
 
   @Id // pk
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   @Column(name = "login_id", length = 80, nullable = false, unique = true)
   private String loginId;
-  @Column(name = "email_address", length = 250, nullable = false)
+  @Column(name = "email_address", length = 250, nullable = false, unique = true)
   private String emailAddress;
   @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // Json 결과로 출력 안 할 데이터
   @Column(name = "password", length = 512, nullable = false)
@@ -47,32 +47,21 @@ public class MemberEntity extends BaseTimeEntity implements UserDetails {
   private String nickName;
   @Column(name = "birthday")
   private Date birthday;
-  @Column(name = "homepage", length = 250)
-  private String homepage;
-  @Column(name = "blog", length = 250)
-  private String blog;
-  @Column(name = "phone_number", length = 45, nullable = false)
-  private String phoneNumber;
-  @Column(name = "student_id", length = 45, nullable = false)
+  @Column(name = "student_id", length = 45, nullable = false, unique = true)
   private String studentId;
-  //  @Column(name = "register_date")
-  //  @Temporal(TemporalType.TIMESTAMP) // 년 월 일 시 분 초의 Date 타입
-  //  private Date registerDate;
   @Column(name = "member_type_id")
   private int memberTypeId;
   @Column(name = "member_rank_id")
   private int memberRankId;
   @Column(name = "point", nullable = false)
   private int point;
-  @Column(name = "level")
+  @Column(name = "level", nullable = false)
   private int level;
 
   @PrePersist // persist 되기 전에 호출되는 함수, 생성할 때 0으로 고정이므로 대입, 입력되는 값이라면 확인이 필요
   public void prePersist() {
     this.memberTypeId = 1;
     this.memberRankId = 1;
-    this.point = 0;
-    this.level = 0;
   }
 
   @ElementCollection(fetch = FetchType.EAGER)
