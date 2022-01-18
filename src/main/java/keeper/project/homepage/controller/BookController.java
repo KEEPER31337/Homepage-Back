586d6eff9b7bdc7c41a71fb09dd25838c7bcc868
1004-head <woduf0628@naver.com>
2022-01-18 17:34:33 +0900

    feat : 기존 책에 추가하는 코드와 에러 처리 코드 추가
    delete에서 책 정보 가져오는걸 조건문 내부로 이동
    
    issue : #11

--- src/main/java/keeper/project/homepage/controller/BookController.java
+++ src/main/java/keeper/project/homepage/controller/BookController.java
@@ -1,19 +1,15 @@
 package keeper.project.homepage.controller;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Optional;
 import keeper.project.homepage.dto.CommonResult;
 import keeper.project.homepage.entity.BookEntity;
-import keeper.project.homepage.exception.CustomMemberNotFoundException;
 import keeper.project.homepage.repository.BookRepository;
 import keeper.project.homepage.service.BookManageService;
 import keeper.project.homepage.service.ResponseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.apache.ibatis.annotations.Update;
-import org.springframework.beans.propertyeditors.CustomDateEditor;
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,31 +26,32 @@ public class BookController {
   private final BookRepository bookRepository;
   private final ResponseService responseService;
   private final BookManageService bookManageService;
-  private final BookEntity bookEntity;
 
   @PostMapping(value = "/addbook")
   @ResponseBody
   public CommonResult add(
       @RequestParam String title,
       @RequestParam String author,
-      @RequestParam String picture,
-      @RequestParam String information,
+      @RequestParam @Nullable String picture,
+      @RequestParam @Nullable String information,
       @RequestParam Long quantity) {
 
     if(bookManageService.isNotMax(title, quantity)) {
-      bookRepository.save(BookEntity.builder()
+      Long total = bookRepository.findByTitle(title).get().getTotal();
+      bookRepository.save(
+          BookEntity.builder()
           .title(title)
           .author(author)
           .picture(picture)
           .information(information)
-          .total(quantity)
+          .total(total+quantity)
           .borrow(0L)
-          .enable(quantity)
+          .enable(total+quantity)
           .registerDate(new Date())
           .build());
       return responseService.getSuccessResult();
     }else{
-      return responseService.getFailResult();
+      return responseService.getFailResult(-1, "수량 초과입니다.");
     }
   }
 
@@ -62,16 +59,18 @@ public class BookController {
   @ResponseBody
   public CommonResult delete(@RequestParam String title, @RequestParam Long quantity) {
 
-    String author = bookRepository.findByTitle(title).get().getAuthor();
-    String picture = bookRepository.findByTitle(title).get().getPicture();
-    String information = bookRepository.findByTitle(title).get().getInformation();
-    Long borrow = bookRepository.findByTitle(title).get().getBorrow();
-
     if(bookManageService.isCanDelete(title, quantity)){
       if(bookRepository.findByTitle(title).get().getTotal()-quantity == 0){
+        BookEntity bookEntity = bookRepository.findByTitle(title).get();
         bookRepository.delete(bookEntity);
       }else{
-        bookRepository.save(BookEntity.builder()
+        String author = bookRepository.findByTitle(title).get().getAuthor();
+        String picture = bookRepository.findByTitle(title).get().getPicture();
+        String information = bookRepository.findByTitle(title).get().getInformation();
+        Long borrow = bookRepository.findByTitle(title).get().getBorrow();
+
+        bookRepository.save(
+            BookEntity.builder()
             .title(title)
             .author(author)
             .picture(picture)
