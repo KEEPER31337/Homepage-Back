29a9c9b7b8cf8d24eb7a3ae8f2428fb085273208
1004-head <woduf0628@naver.com>
2022-01-18 20:07:14 +0900

    feat : 도서 삭제 테스트 코드 추가
    issue : #11

--- src/test/java/keeper/project/homepage/controller/BookControllerTest.java
+++ src/test/java/keeper/project/homepage/controller/BookControllerTest.java
@@ -179,5 +179,78 @@ public class BookControllerTest {
         .andExpect(jsonPath("$.msg").exists());
   }
 
-  
+  @Test
+  @DisplayName("책 삭제 성공(일부 삭제)")
+  public void deleteBook() throws Exception {
+    Long bookQuantity1 = 1L;
+    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+    params.add("title", bookTitle);
+    params.add("quantity", String.valueOf(bookQuantity1));
+
+    mockMvc.perform(post("/v1/deletebook").params(params))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.success").value(true))
+        .andExpect(jsonPath("$.code").value(0))
+        .andExpect(jsonPath("$.msg").exists())
+        .andDo(document("delete-book",
+            requestParameters(
+                parameterWithName("title").description("책 제목"),
+                parameterWithName("quantity").description("삭제 할 수량")
+            ),
+            responseFields(
+                fieldWithPath("success").description("책 삭제 완료 시 true, 실패 시 false 값을 보냅니다."),
+                fieldWithPath("code").description("책 삭제 완료 시 0, 최대 수량 초과로 실패 시 -1, 없는 책으로 실패 시 -2 코드를 보냅니다."),
+                fieldWithPath("msg").description("책 삭제 실패가 수량 초과 일 때 수량 초과 메시지를, 없는 책일 때 책이 없다는 메시지를 발생시킵니다.")
+            )));
+  }
+
+  @Test
+  @DisplayName("책 삭제 성공(전체 삭제)")
+  public void deleteBookMax() throws Exception {
+    Long bookQuantity3 = 2L;
+    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+    params.add("title", bookTitle);
+    params.add("quantity", String.valueOf(bookQuantity3));
+
+    mockMvc.perform(post("/v1/deletebook").params(params))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.success").value(true))
+        .andExpect(jsonPath("$.code").value(0))
+        .andExpect(jsonPath("$.msg").exists());
+  }
+
+  @Test
+  @DisplayName("책 삭제 실패(없는 책)")
+  public void deleteBookFailedNoExist() throws Exception {
+    Long bookQuantity3 = 1L;
+    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+    params.add("title", bookTitle+epochTime);
+    params.add("author", bookAuthor);
+    params.add("quantity", String.valueOf(bookQuantity3));
+
+    mockMvc.perform(post("/v1/deletebook").params(params))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.success").value(false))
+        .andExpect(jsonPath("$.code").value(-2))
+        .andExpect(jsonPath("$.msg").exists());
+  }
+
+  @Test
+  @DisplayName("책 삭제 실패(기존보다 많은 수량)")
+  public void deleteBookFailedOverMax() throws Exception {
+    Long bookQuantity3 = 5L;
+    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+    params.add("title", bookTitle);
+    params.add("quantity", String.valueOf(bookQuantity3));
+
+    mockMvc.perform(post("/v1/deletebook").params(params))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.success").value(false))
+        .andExpect(jsonPath("$.code").value(-1))
+        .andExpect(jsonPath("$.msg").exists());
+  }
 }
