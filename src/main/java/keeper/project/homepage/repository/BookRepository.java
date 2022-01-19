a4c023413edef6968f3d26a77920d596ca9f9ffc
1004-head <woduf0628@naver.com>
2022-01-19 13:36:34 +0900

    fix : repository에 repository annotation 적용
    이걸 왜 빼먹고 있었지...
    
    issue : #11

--- src/main/java/keeper/project/homepage/repository/BookRepository.java
+++ src/main/java/keeper/project/homepage/repository/BookRepository.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import keeper.project.homepage.entity.BookEntity;
 import lombok.Getter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<BookEntity, Long> {
 
   Optional<BookEntity> findByTitle(String title);
