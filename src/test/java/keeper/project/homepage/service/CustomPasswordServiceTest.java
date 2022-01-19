a6ff646ca696453e6ce4bb6a6cc115ce9d1249ca
정핸모 <gusah009@naver.com>
2022-01-19 23:26:30 +0900

    Merge pull request #18 from KEEPER31337/feature/구DB_패스워드_동기화
    Feature/구db 패스워드 동기화
    
    (cherry picked from commit 424fbd0a592d843751ab1433f6801bc3afb27b90)

---
+++ src/test/java/keeper/project/homepage/service/CustomPasswordServiceTest.java
@@ -0,0 +1,68 @@
+package keeper.project.homepage.service;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomPasswordServiceTest {
+
+  final private String password = "test123";
+
+  @Autowired
+  CustomPasswordService customPasswordService;
+
+  @Test
+  @DisplayName("구DB pbkdf2_sha256 비밀번호 검증 성공")
+  public void passwordMatchWithSHA256() throws Exception {
+    String exceptString = customPasswordService.encodeWithPBKDF2SHA256(password, "2ygQ3QEXOkbX",
+        512);
+    // then
+    assertTrue(customPasswordService.checkPasswordWithPBKDF2SHA256(password,
+        "sha256:0000512:2ygQ3QEXOkbX:NTJZJFeOsNOmmCAQ3bKhqFX2BwQzXtIs"));
+  }
+
+  @Test
+  @DisplayName("구DB pbkdf2_sha256 비밀번호 검증 실패")
+  public void passwordDisMatchWithSHA256() throws Exception {
+    String exceptString = customPasswordService.encodeWithPBKDF2SHA256(password, "2ygQ3QEXOkbX",
+        512);
+    // then
+    assertFalse(customPasswordService.checkPasswordWithPBKDF2SHA256(password,
+        "sha256:0000512:2ygQ3QEXOkbX:KeeperMo"));
+    assertFalse(customPasswordService.checkPasswordWithPBKDF2SHA256(password,
+        "sha256:0000512:KeeperMo:NTJZJFeOsNOmmCAQ3bKhqFX2BwQzXtIs"));
+    assertFalse(customPasswordService.checkPasswordWithPBKDF2SHA256(password,
+        "sha256:KeeperMo:2ygQ3QEXOkbX:NTJZJFeOsNOmmCAQ3bKhqFX2BwQzXtIs"));
+    /*
+     *  SHA1이 따로 존재 할 경우 처리할 Code
+     *  assertFalse(customPasswordService.checkPasswordWithPBKDF2SHA256(password,
+     *    "KeeperMo:0000512:2ygQ3QEXOkbX:NTJZJFeOsNOmmCAQ3bKhqFX2BwQzXtIs"));
+     */
+    assertFalse(customPasswordService.checkPasswordWithPBKDF2SHA256(password,
+        "1:2:하나가 없을 때"));
+  }
+
+
+  @Test
+  @DisplayName("구DB md5 비밀번호 검증 성공")
+  public void passwordMatchWithMD5() throws Exception {
+    String exceptString = customPasswordService.encodeWithMD5(password);
+    // then
+    assertTrue(customPasswordService.checkPasswordWithMD5(password,
+        "cc03e747a6afbbcbf8be7668acfebee5"));
+  }
+
+  @Test
+  @DisplayName("구DB md5 비밀번호 검증 실패")
+  public void passwordDisMatchWithMD5() throws Exception {
+    String exceptString = customPasswordService.encodeWithMD5(password);
+    // then
+    assertFalse(customPasswordService.checkPasswordWithMD5(password,
+        "keeperMo"));
+  }
+}
