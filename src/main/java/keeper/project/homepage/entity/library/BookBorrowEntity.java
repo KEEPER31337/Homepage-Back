64d3be3745cadef0ec08af2a7f9f7ce73af1856c
1004-head <woduf0628@naver.com>
2022-01-30 17:15:37 +0900

    fix : fetchtype을 eager에서 lazy로 바꿈
    멤버 객체를 가져올 때 전부 불러오는게 아니라 lazy로 프록시 객체를 가져오는게 예상치 못 한 에러를 줄일 수 있다.
    
    issue : #11 #40

--- src/main/java/keeper/project/homepage/entity/library/BookBorrowEntity.java
+++ src/main/java/keeper/project/homepage/entity/library/BookBorrowEntity.java
@@ -34,7 +34,7 @@ public class BookBorrowEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
-  @ManyToOne(targetEntity = MemberEntity.class, fetch = FetchType.EAGER)
+  @ManyToOne(targetEntity = MemberEntity.class, fetch = FetchType.LAZY)
   // 한명의 유저는 여러개의 책 대여 가능이므로 1 : N 관계
   @JoinColumn(name = "member_id") // foreign key 매핑
   private MemberEntity memberId;
