e94f92fd72834421f8f8ebad1876add7022df67e
최우창 <clllickme@naver.com>
2022-02-07 17:54:46 +0900

    feat : 포인트 선물 기능
    - 포인트 선물과 관련된 Service, Controller 생성
    
    issue : #63

--- src/main/java/keeper/project/homepage/controller/member/MemberController.java
+++ src/main/java/keeper/project/homepage/controller/member/MemberController.java
@@ -1,17 +1,23 @@
 package keeper.project.homepage.controller.member;
 
+import keeper.project.homepage.dto.request.PointTransferRequest;
 import keeper.project.homepage.dto.result.ListResult;
+import keeper.project.homepage.dto.result.PointTransferResult;
 import keeper.project.homepage.dto.result.SingleResult;
 import keeper.project.homepage.entity.member.MemberEntity;
 import keeper.project.homepage.exception.CustomMemberNotFoundException;
 import keeper.project.homepage.repository.member.MemberRepository;
 import keeper.project.homepage.service.ResponseService;
+import keeper.project.homepage.service.member.MemberService;
+import keeper.project.homepage.service.util.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +29,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
   private final MemberRepository memberRepository;
+  private final MemberService memberService;
   private final ResponseService responseService;
+  private final AuthService authService;
 
   @Secured("ROLE_회장") // 각 리소스별 권한 설정
   @GetMapping(value = "/members")
@@ -41,5 +49,13 @@ public class MemberController {
     // 결과데이터가 단일건인경우 getSuccessSingleResult 이용해서 결과를 출력한다.
     return responseService.getSuccessSingleResult(
         memberRepository.findById(id).orElseThrow(CustomMemberNotFoundException::new));
+  }
+
+  @Secured("ROLE_회원")
+  @PutMapping("/member/update/point/transfer")
+  public SingleResult<PointTransferResult> transferPoint(
+      @RequestBody PointTransferRequest pointTransferRequest
+  ) {
+    return responseService.getSuccessSingleResult(memberService.transferPoint(pointTransferRequest));
   }
 }
--- src/main/java/keeper/project/homepage/service/member/MemberService.java
+++ src/main/java/keeper/project/homepage/service/member/MemberService.java
@@ -1,10 +1,14 @@
 package keeper.project.homepage.service.member;
 
 import java.util.Optional;
+import keeper.project.homepage.dto.member.MemberDto;
+import keeper.project.homepage.dto.request.PointTransferRequest;
+import keeper.project.homepage.dto.result.PointTransferResult;
 import keeper.project.homepage.entity.member.MemberEntity;
+import keeper.project.homepage.exception.CustomMemberNotFoundException;
+import keeper.project.homepage.exception.CustomTransferPointLackException;
 import keeper.project.homepage.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,5 +20,42 @@ public class MemberService {
   public MemberEntity findById(Long id) throws RuntimeException {
     Optional<MemberEntity> memberEntity = memberRepository.findById(id);
     return memberEntity.orElse(null);
+  }
+
+  public PointTransferResult transferPoint(PointTransferRequest pointTransferRequest) {
+    MemberEntity senderMember = memberRepository.findById(pointTransferRequest.getSenderId())
+        .orElseThrow(CustomMemberNotFoundException::new);
+    MemberEntity receiverMember = memberRepository.findById(pointTransferRequest.getReceiverId())
+        .orElseThrow(CustomMemberNotFoundException::new);
+
+    if (senderMember.getPoint() < pointTransferRequest.getTransmissionPoint()) {
+      throw new CustomTransferPointLackException("잔여 포인트가 부족합니다.");
+    }
+
+    MemberDto senderDto = updateSenderPoint(senderMember,
+        pointTransferRequest.getTransmissionPoint());
+    MemberDto receiverDto = updateReceiverPoint(receiverMember,
+        pointTransferRequest.getTransmissionPoint());
+
+    return new PointTransferResult(pointTransferRequest,
+        senderDto, receiverDto);
+  }
+
+  public MemberDto updateSenderPoint(MemberEntity member, int point) {
+    int remainingPoint = member.getPoint();
+    member.updatePoint(remainingPoint - point);
+
+    MemberDto result = new MemberDto();
+    result.initWithEntity(member);
+    return result;
+  }
+
+  public MemberDto updateReceiverPoint(MemberEntity member, int point) {
+    int remainingPoint = member.getPoint();
+    member.updatePoint(remainingPoint + point);
+
+    MemberDto result = new MemberDto();
+    result.initWithEntity(member);
+    return result;
   }
 }
