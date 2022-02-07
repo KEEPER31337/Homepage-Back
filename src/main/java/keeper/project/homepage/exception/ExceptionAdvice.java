49418cbae04ac48f79388e1d645651788ae44ac3
최우창 <clllickme@naver.com>
2022-02-07 17:51:56 +0900

    feat : 포인트 Exception 처리 생성
    - 포인트 부족과 관련된 Exception 생성
    
    issue : #63

--- src/main/java/keeper/project/homepage/exception/ExceptionAdvice.java
+++ src/main/java/keeper/project/homepage/exception/ExceptionAdvice.java
@@ -96,4 +96,12 @@ public class ExceptionAdvice {
     return responseService.getFailResult(Integer.parseInt(getMessage("fileNotFound.code")),
         e.getMessage() == null ? getMessage("fileNotFound.msg") : e.getMessage());
   }
+
+  @ExceptionHandler(CustomTransferPointLackException.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public CommonResult transferPointLackException(HttpServletRequest request,
+      CustomTransferPointLackException e) {
+    return responseService.getFailResult(Integer.parseInt(getMessage("pointLackException.code")),
+        e.getMessage() == null ? getMessage("pointLackException.msg") : e.getMessage());
+  }
 }
