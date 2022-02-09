92faedde19b315b65389c40d0a25305117659137
1004-head <woduf0628@naver.com>
2022-02-09 15:04:45 +0900

    feat : 최근 추가 도서 표시 기능
    issue : #10 #44

---
+++ src/main/java/keeper/project/homepage/service/library/LibraryMainService.java
@@ -0,0 +1,32 @@
+package keeper.project.homepage.service.library;
+
+import java.util.ArrayList;
+import java.util.List;
+import keeper.project.homepage.entity.library.BookEntity;
+import keeper.project.homepage.repository.library.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LibraryMainService {
+
+  private final BookRepository bookRepository;
+
+  public List<BookEntity> displayTenBooks(){
+    List<BookEntity> bookEntityList =new ArrayList<>();
+
+    Sort sort = Sort.by("id").descending();
+    Pageable pageable = PageRequest.of(0, 10, sort);
+    Page<BookEntity> bookEntityPage = bookRepository.findAll(pageable);
+
+    for(BookEntity bookEntity : bookEntityPage.getContent()){
+      bookEntityList.add(bookEntity);
+    }
+    return bookEntityList;
+  }
+}
