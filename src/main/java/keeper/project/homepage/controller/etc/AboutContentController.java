ed0f9b276bbfee44a9f9f8b2787ce83e6cd20c48
최우창 <clllickme@naver.com>
2022-02-11 16:22:20 +0900

    feat : StaticWriteTitle CRUD 기능 구현
    - Dto를 통한 Service 로직 생성
    - Service 로직을 이용한 Controller 로직 생성
    
    issue : #31

--- src/main/java/keeper/project/homepage/controller/etc/AboutContentController.java
+++ src/main/java/keeper/project/homepage/controller/etc/AboutContentController.java
@@ -1,22 +1,8 @@
 package keeper.project.homepage.controller.etc;
 
-import keeper.project.homepage.dto.etc.StaticWriteTitleDto;
-import keeper.project.homepage.dto.result.CommonResult;
-import keeper.project.homepage.dto.result.ListResult;
-import keeper.project.homepage.dto.result.SingleResult;
-import keeper.project.homepage.entity.etc.StaticWriteSubtitleImageEntity;
-import keeper.project.homepage.entity.etc.StaticWriteTitleEntity;
-import keeper.project.homepage.service.ResponseService;
-import keeper.project.homepage.service.etc.AboutService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
