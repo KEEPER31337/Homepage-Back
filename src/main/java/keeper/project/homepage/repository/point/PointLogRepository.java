b3f09c6afbd23b59955dcfb8ffd057840ae4ee50
최우창 <clllickme@naver.com>
2022-02-14 19:32:05 +0900

    feat : 포인트 관리 로직 생성
    - 포인트 관련 로직을 위해 Repository, Service, Controller 코드 작성
    
    issue : #59

---
+++ src/main/java/keeper/project/homepage/repository/point/PointLogRepository.java
@@ -0,0 +1,17 @@
+package keeper.project.homepage.repository.point;
+
+import java.util.List;
+import keeper.project.homepage.entity.member.MemberEntity;
+import keeper.project.homepage.entity.point.PointLogEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface PointLogRepository extends JpaRepository<PointLogEntity, Long> {
+
+  List<PointLogEntity> findAllByMemberAndPresentedMemberIsNull(MemberEntity memberEntity, Pageable pageable);
+
+  List<PointLogEntity> findAllByMemberAndPresentedMemberIsNotNull(MemberEntity memberEntity, Pageable pageable);
+
+  List<PointLogEntity> findAllByPresentedMemberAndMemberIsNotNull(MemberEntity memberEntity, Pageable pageable);
+}
