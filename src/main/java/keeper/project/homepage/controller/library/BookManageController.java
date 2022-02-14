f833885d462e44278f740a3ea5278ec1873cb883
1004-head <woduf0628@naver.com>
2022-02-14 13:21:33 +0900

    fix : BookManageController 충돌 해결
    브랜치 최신화 하며 생긴 충돌 해결
    
    issue : #80

--- src/main/java/keeper/project/homepage/controller/library/BookManageController.java
+++ src/main/java/keeper/project/homepage/controller/library/BookManageController.java
@@ -6,12 +6,9 @@ import javax.servlet.http.HttpServletRequest;
 import keeper.project.homepage.common.ImageCenterCrop;
 import keeper.project.homepage.dto.library.BookDto;
 import keeper.project.homepage.dto.result.CommonResult;
-<<<<<<< HEAD
 import keeper.project.homepage.entity.FileEntity;
 import keeper.project.homepage.entity.ThumbnailEntity;
-=======
 import keeper.project.homepage.dto.result.ListResult;
->>>>>>> 4c9a008226c4b116b98e7f0895b244cd145d175e
 import keeper.project.homepage.entity.library.BookBorrowEntity;
 import keeper.project.homepage.entity.library.BookEntity;
 import keeper.project.homepage.exception.CustomAboutFailedException;
@@ -53,17 +50,12 @@ public class BookManageController {
   private final AuthService authService;
   private final FileService fileService;
   private final ThumbnailService thumbnailService;
+  private final ResponseService responseService;
 
   @Secured({"ROLE_사서", "ROLE_회장"})
   @GetMapping(value = "/overduebooks")
-<<<<<<< HEAD
-  public ResponseEntity<List<BookBorrowEntity>> sendOverdueBooks(
-      @PageableDefault(size = 10, sort = "expireDate", direction = Direction.ASC)
-          Pageable pageable) {
-=======
   public ListResult<BookBorrowEntity> sendOverdueBooks(@PageableDefault(size = 10, sort = "expireDate", direction = Direction.ASC)
       Pageable pageable){
->>>>>>> 4c9a008226c4b116b98e7f0895b244cd145d175e
 
     return responseService.getSuccessListResult(bookManageService.sendOverdueBooks(pageable));
   }
