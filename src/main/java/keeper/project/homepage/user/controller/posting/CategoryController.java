356e8e23d3e3a1d5a466e2d0414100453e997711
최우창 <clllickme@naver.com>
2022-02-19 02:38:46 +0900

    feat : user Category Service, Controller
    - 관리자가 아니여도 접근할 수 있는 카테고리 리스트 불러오는 로직 수정
    
    issue : #32

---
+++ src/main/java/keeper/project/homepage/user/controller/posting/CategoryController.java
@@ -0,0 +1,49 @@
+package keeper.project.homepage.user.controller.posting;
+
+import keeper.project.homepage.dto.posting.category.result.CategoryResult;
+import keeper.project.homepage.dto.posting.category.result.CategoryWithChildResult;
+import keeper.project.homepage.dto.result.ListResult;
+import keeper.project.homepage.service.ResponseService;
+import keeper.project.homepage.user.service.posting.CategoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/v1/category")
+public class CategoryController {
+
+  private final CategoryService categoryService;
+  private final ResponseService responseService;
+
+  @GetMapping("/lists/head/all")
+  public ListResult<CategoryWithChildResult> getAllHeadCategoryAndChild() {
+    return responseService.getSuccessListResult(categoryService.getAllHeadCategoryAndChild());
+  }
+
+  @GetMapping("/lists/head")
+  public ListResult<CategoryResult> getAllHeadCategory() {
+    return responseService.getSuccessListResult(categoryService.getAllHeadCategory());
+  }
+
+  @GetMapping("/lists/all/{parentId}")
+  public ListResult<CategoryWithChildResult> getAllCategoryAndChildByParentId(
+      @PathVariable("parentId") Long parentId
+  ) {
+    return responseService.getSuccessListResult(
+        categoryService.getAllCategoryAndChildByParentId(parentId));
+  }
+
+  @GetMapping("/lists/{parentId}")
+  public ListResult<CategoryResult> getAllCategoryByParentId(
+      @PathVariable("parentId") Long parentId
+  ) {
+    return responseService.getSuccessListResult(categoryService.getAllCategoryByParentId(parentId));
+  }
+
+}
