18f2565a03388b37629264c84b4915b99d5830bb
정핸모 <gusah009@naver.com>
2022-02-19 09:33:35 +0900

    fix: 랭킹 수정사항
    - 오타 수정
    - page와 size 모두 null일 경우 모든 정보 불러오기
    - rank dto 요청사항에 맞게 수정
    - 로그인하지 않은 사용자는 랭킹 열람이 불가능하게 수정

--- src/test/java/keeper/project/homepage/controller/attendance/RankControllerTest.java
+++ src/test/java/keeper/project/homepage/controller/attendance/RankControllerTest.java
@@ -7,9 +7,11 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.subsecti
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import java.util.ArrayList;
 import java.util.List;
+import keeper.project.homepage.ApiControllerTestHelper;
 import keeper.project.homepage.ApiControllerTestSetUp;
 import keeper.project.homepage.entity.member.MemberEntity;
 import keeper.project.homepage.entity.member.MemberHasMemberJobEntity;
@@ -23,9 +25,10 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
 
+
 @Transactional
 @Log4j2
-public class RankControllerTest extends ApiControllerTestSetUp {
+public class RankControllerTest extends ApiControllerTestHelper {
 
   final private String loginId = "hyeonmomo";
   final private String password = "keeper";
@@ -36,21 +39,25 @@ public class RankControllerTest extends ApiControllerTestSetUp {
 
   private MemberEntity memberEntity1, memberEntity2, memberEntity3;
 
+  private String userToken;
+
   @BeforeEach
   public void setUp() throws Exception {
 
     memberEntity1 = generateTestMember(2000);
     memberEntity2 = generateTestMember(3000);
     memberEntity3 = generateTestMember(1000);
+    userToken = generateJWTToken(memberEntity1.getLoginId(), password);
   }
 
   @Test
-  @DisplayName("전체 랭킹 출력")
+  @DisplayName("페이징 랭킹 출력")
   public void showRanking() throws Exception {
     ResultActions result = mockMvc.perform(
         MockMvcRequestBuilders.get("/v1/rank")
+            .header("Authorization", userToken)
             .param("page", "0")
-            .param("size", "2")
+            .param("size", "3")
     );
 
     result.andExpect(MockMvcResultMatchers.status().isOk())
@@ -58,31 +65,37 @@ public class RankControllerTest extends ApiControllerTestSetUp {
         .andDo(document("rank-list",
             requestParameters(
                 parameterWithName("page").optional().description("페이지 번호(default = 0)"),
-                parameterWithName("size").optional().description("한 페이지당 출력 수(default = 25)")
+                parameterWithName("size").optional().description("한 페이지당 출력 수")
             ),
             responseFields(
                 fieldWithPath("success").description("에러 발생이 아니면 항상 true"),
                 fieldWithPath("code").description("에러 발생이 아니면 항상 0"),
                 fieldWithPath("msg").description("에러 발생이 아니면 항상 성공하였습니다"),
                 fieldWithPath("list[].id").description("멤버 ID"),
-                fieldWithPath("list[].emailAddress").description("이메일 주소"),
                 fieldWithPath("list[].nickName").description("닉네임"),
-                fieldWithPath("list[].birthday").description("생일"),
-                fieldWithPath("list[].registerDate").description("회원가입 날짜"),
-                subsectionWithPath("list[].memberType").description("멤버 타입").optional(), // 탈퇴회원 때문
-                subsectionWithPath("list[].memberRank").description("멤버 랭크 타입").optional(),
+                fieldWithPath("list[].thumbnailId").description("멤버 썸네일").optional(),
+                fieldWithPath("list[].jobs[]").description("멤버 직책"),
                 fieldWithPath("list[].point").description("포인트"),
-                fieldWithPath("list[].level").description("레벨"),
-                fieldWithPath("list[].thumbnail").description("멤버 썸네일"),
-                fieldWithPath("list[].follower").description("팔로워"),
-                fieldWithPath("list[].followee").description("팔로잉"),
-                subsectionWithPath("list[].memberJobs").description("멤버 직책"),
-                subsectionWithPath("list[].posting").description("작성한 게시물"),
-                subsectionWithPath("list[].authorities").description("멤버 권한")
+                fieldWithPath("list[].rank").description("포인트 등수")
             )));
   }
 
-  private MemberEntity generateTestMember(int point) {
+  @Test
+  @DisplayName("전체 랭킹 출력")
+  public void showRankingAll() throws Exception {
+    int memberCount = memberRepository.findAll().size();
+
+    ResultActions result = mockMvc.perform(
+        MockMvcRequestBuilders.get("/v1/rank")
+//            .param("page", "0")
+//            .param("size", "2")
+    );
+
+    result.andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(jsonPath("$.list.length()").value(memberCount));
+  }
+
+  private MemberEntity generateTestMember(int point) throws Exception {
     final long epochTime = System.nanoTime();
     MemberJobEntity memberJobEntity = memberJobRepository.findByName("ROLE_회원").get();
     MemberHasMemberJobEntity hasMemberJobEntity = MemberHasMemberJobEntity.builder()
