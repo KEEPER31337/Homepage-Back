82a24b03a5e1043a11541ff18c6cbd358cc351e6
최우창 <clllickme@naver.com>
2022-02-23 23:45:37 +0900

    fix : admin About Controller 생성
    - admin, user 분리에 의한 Admin Controller 로직 분리
    
    issue : #31, #32

---
+++ src/main/java/keeper/project/homepage/admin/controller/about/AdminAboutContentController.java
@@ -0,0 +1,65 @@
+package keeper.project.homepage.admin.controller.about;
+
+import keeper.project.homepage.admin.service.about.AdminAboutContentService;
+import keeper.project.homepage.dto.etc.StaticWriteContentDto;
+import keeper.project.homepage.dto.result.SingleResult;
+import keeper.project.homepage.dto.result.StaticWriteContentResult;
+import keeper.project.homepage.service.ResponseService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/v1/admin/about/content")
+public class AdminAboutContentController {
+
+  private final AdminAboutContentService adminAboutContentService;
+  private final ResponseService responseService;
+
+  @Secured("ROLE_회장")
+  @PostMapping(value = "/create")
+  public SingleResult<StaticWriteContentResult> createContent(
+      @RequestBody StaticWriteContentDto staticWriteContentDto
+  ) {
+
+    return responseService.getSuccessSingleResult(
+        adminAboutContentService.createContent(staticWriteContentDto));
+  }
+
+  @Secured("ROLE_회장")
+  @DeleteMapping(value = "/delete/{id}")
+  public SingleResult<StaticWriteContentResult> deleteContentById(
+      @PathVariable("id") Long id
+  ) {
+
+    return responseService.getSuccessSingleResult(
+        adminAboutContentService.deleteContentById(id)
+    );
+  }
+
+  @Secured("ROLE_회장")
+  @RequestMapping(
+      method = {RequestMethod.PUT, RequestMethod.PATCH},
+      value = "/modify/{id}",
+      produces = {MediaType.APPLICATION_JSON_VALUE})
+  public SingleResult<StaticWriteContentResult> modifyContentById(
+      @PathVariable("id") Long id,
+      @RequestBody StaticWriteContentDto staticWriteContentDto
+  ) {
+
+    return responseService.getSuccessSingleResult(
+        adminAboutContentService.modifyContentById(staticWriteContentDto, id)
+    );
+  }
+
+}
