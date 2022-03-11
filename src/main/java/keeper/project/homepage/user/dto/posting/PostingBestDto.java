78b168859516b11244c68c3dccee08bfae5c2045
JCW <codnjs8221@naver.com>
2022-03-11 21:38:43 +0900

    fix : PostingBestdto에 썸네일 조회 url 정보를 추가
    - 썸네일의 id가 아니라 썸네일 조회 api path를 반환하도록 수정
    
    issue : #192

--- src/main/java/keeper/project/homepage/user/dto/posting/PostingBestDto.java
+++ src/main/java/keeper/project/homepage/user/dto/posting/PostingBestDto.java
@@ -1,6 +1,7 @@
 package keeper.project.homepage.user.dto.posting;
 
 import java.time.LocalDateTime;
+import keeper.project.homepage.common.controller.util.ImageController;
 import keeper.project.homepage.entity.posting.PostingEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -18,26 +19,26 @@ public class PostingBestDto {
   private Long id;
   private String title;
   private String user;
-  private Long userThumbnailID;
+  private String userThumbnailPath;
   private LocalDateTime dateTime;
   private Integer watch;
   private Integer commentN;
   private Long categoryId;
   private String category;
-  private Long ThumbnailId;
+  private String thumbnailPath;
 
   public void initWithEntity(PostingEntity postingEntity) {
     this.id = postingEntity.getId();
     this.title = postingEntity.getTitle();
     this.user = postingEntity.getWriter();
-    this.userThumbnailID = postingEntity.getWriterThumbnailId();
+    this.userThumbnailPath = ImageController.THUMBNAIL_PATH + postingEntity.getWriterThumbnailId();
     this.dateTime = postingEntity.getRegisterTime();
     this.watch = postingEntity.getVisitCount();
     this.commentN = postingEntity.getCommentCount();
     this.categoryId = postingEntity.getCategoryId().getId();
     this.category = postingEntity.getCategoryId().getName();
     if (postingEntity.getThumbnail() != null) {
-      this.ThumbnailId = postingEntity.getThumbnail().getId();
+      this.thumbnailPath = ImageController.THUMBNAIL_PATH + postingEntity.getThumbnail().getId();
     }
   }
 }
