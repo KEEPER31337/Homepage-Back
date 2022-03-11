43fc504ddf6ca7debeed6ffbc74fee21ee0b1809
JCW <codnjs8221@naver.com>
2022-03-11 20:59:07 +0900

    fix : Book dto에 썸네일 조회 url 정보를 추가
    - 썸네일의 id가 아니라 썸네일 조회 api path를 반환하도록 수정
    
    issue : #192

--- src/main/java/keeper/project/homepage/user/dto/library/BookResult.java
+++ src/main/java/keeper/project/homepage/user/dto/library/BookResult.java
@@ -2,6 +2,7 @@ package keeper.project.homepage.user.dto.library;
 
 import java.time.LocalDateTime;
 import java.util.Date;
+import keeper.project.homepage.common.controller.util.ImageController;
 import keeper.project.homepage.entity.library.BookEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,7 +26,7 @@ public class BookResult {
   private Long borrow;
   private Long enable;
   private Date registerDate;
-  private Long thumbnailId;
+  private String thumbnailPath;
 
   public void initWithEntity(BookEntity bookEntity) {
     this.id = bookEntity.getId();
@@ -40,7 +41,7 @@ public class BookResult {
     this.enable = bookEntity.getEnable();
     this.registerDate = bookEntity.getRegisterDate();
     if (bookEntity.getThumbnailId() != null) {
-      this.thumbnailId = bookEntity.getThumbnailId().getId();
+      this.thumbnailPath = ImageController.THUMBNAIL_PATH + bookEntity.getThumbnailId().getId();
     }
 
   }
