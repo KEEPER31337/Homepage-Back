8c6c890ab51123dbd70936cea1828aebcdaa107b
JCW <codnjs8221@naver.com>
2022-03-19 18:45:25 +0900

    fix : thumbnail path 하드 코딩 하던 부분 수정

--- src/main/java/keeper/project/homepage/util/EnvironmentProperty.java
+++ src/main/java/keeper/project/homepage/util/EnvironmentProperty.java
@@ -1,37 +1,17 @@
 package keeper.project.homepage.util;
 
+import java.net.URI;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 public class EnvironmentProperty {
 
   private static final String THUMBNAIL_PATH = "/v1/util/thumbnail/";
-  private static final String SERVER_IP = "3.34.46.234";
-
-  private enum URL_TYPE {
-    Local("127.0.0.1", "8080"),
-    Dev(SERVER_IP, "3000"),
-    Prod(SERVER_IP, "8080");
-
-    String ip;
-    String port;
-
-    URL_TYPE(String ip, String port) {
-      this.ip = ip;
-      this.port = port;
-    }
-
-    public String getIp() {
-      return ip;
-    }
-
-    public String getPort() {
-      return port;
-    }
-  }
-
-  private static String getURL(URL_TYPE type) {
-    return "http://" + type.getIp() + ":" + type.getPort();
-  }
 
   public static String getThumbnailPath(Long thumbnailId) {
-    return getURL(URL_TYPE.Prod) + THUMBNAIL_PATH + thumbnailId;
+    URI location = ServletUriComponentsBuilder.fromCurrentContextPath()
+        .path(THUMBNAIL_PATH + "/{id}")
+        .buildAndExpand(thumbnailId)
+        .toUri();
+    return location.toString();
   }
 }
