182fd12bb2827f062ce5b43be15a0ce7f1deac19
최우창 <clllickme@naver.com>
2022-03-20 12:26:50 +0900

    fix: 팔로우, 언팔로우 시 로직 변경(로그인 ID가 아닌 해당 멤버의 ID를 통해 진행)

--- src/test/java/keeper/project/homepage/user/controller/member/MemberControllerTest.java
+++ src/test/java/keeper/project/homepage/user/controller/member/MemberControllerTest.java
@@ -2,12 +2,9 @@ package keeper.project.homepage.user.controller.member;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.payload.PayloadDocumentation.subsectionWithPath;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
