57471785bfd1118211908d14b30a600e686e3bbe
Jun Seol <physiogel@pusan.ac.kr>
2022-05-17 21:39:54 +0900

    fix: dockerfile 정리

--- docker/dockerfile/app.dockerfile
+++
@@ -1,6 +0,0 @@
-FROM gradle:7.3.2-jdk17
-
-WORKDIR /home/keeper
-
-COPY ../config/application.properties /application.properties
-COPY ../config/entrypoint.sh /entrypoint.sh
--- docker/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build -t keeper-homepage-app:test -f dockerfile/app.dockerfile .
-docker build --platform=linux/amd64 -t keeper-homepage-db:test -f dockerfile/db.dockerfile .
-
-docker-compose -p keeper up
