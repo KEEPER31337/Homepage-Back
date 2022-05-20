65291f8d369710fe007c85a3442373ebfd247b2e
최우창 <clllickme@naver.com>
2022-05-20 14:37:50 +0900

    feat: 충돌해결
    issue: #219

--- src/test/java/keeper/project/homepage/admin/controller/about/AdminStaticWriteTitleControllerTest.java
+++ src/test/java/keeper/project/homepage/admin/controller/about/AdminStaticWriteTitleControllerTest.java
@@ -110,7 +110,7 @@ public class AdminStaticWriteTitleControllerTest extends AdminStaticWriteTestHel
         .andDo(print())
         .andExpect(status().is4xxClientError())
         .andExpect(jsonPath("$.success").value(false))
-        .andExpect(jsonPath("$.msg").value("보유한 권한으로 접근할수 없는 리소스 입니다"));
+        .andExpect(jsonPath("$.msg").value("접근이 거부되었습니다."));
   }
 
 }
