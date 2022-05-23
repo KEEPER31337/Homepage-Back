2759274cd14b763d02f967937c07340d1f26d325
정현모 <gusah009@naver.com>
2022-05-23 12:27:04 +0900

    fix: 스터디 수정 API 수정
    - Study 수정 API 사용 시 수정될 멤버 리스트도 모두 보내주게 변경하였습니다.
    
    issue: #294

--- src/main/java/keeper/project/homepage/user/dto/study/ModifyStudyRequestDto.java
+++ src/main/java/keeper/project/homepage/user/dto/study/ModifyStudyRequestDto.java
@@ -1,5 +1,7 @@
 package keeper.project.homepage.user.dto.study;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,4 +10,5 @@ import lombok.Setter;
 public class ModifyStudyRequestDto extends StudyRequestDto {
 
   private Long studyId;
+  private List<Long> memberIdList = new ArrayList<>();
 }
