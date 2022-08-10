3ad598bc9d1b62892543a85f3504a43464b0343d
woo-chang <clllickme@naver.com>
2022-08-10 10:23:31 +0900

    feat: 투표자 현황 조회
    issue: #336

--- src/main/java/keeper/project/homepage/repository/election/ElectionVoterRepository.java
+++ src/main/java/keeper/project/homepage/repository/election/ElectionVoterRepository.java
@@ -1,10 +1,12 @@
 package keeper.project.homepage.repository.election;
 
-import java.util.List;
+import keeper.project.homepage.entity.election.ElectionEntity;
 import keeper.project.homepage.entity.election.ElectionVoterEntity;
 import keeper.project.homepage.entity.election.ElectionVoterPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ElectionVoterRepository extends JpaRepository<ElectionVoterEntity, ElectionVoterPK> {
+
+  Integer countAllByElectionVoterPK_ElectionAndIsVotedIsTrue(ElectionEntity election);
 
 }
