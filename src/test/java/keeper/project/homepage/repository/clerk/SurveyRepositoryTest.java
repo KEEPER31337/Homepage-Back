8249b0e854b9c524320983e8e8162d257e0e2602
shkisme <shkisme0130@gmail.com>
2022-08-18 16:13:46 +0900

    feat: repository test code 작성

---
+++ src/test/java/keeper/project/homepage/repository/clerk/SurveyRepositoryTest.java
@@ -0,0 +1,62 @@
+package keeper.project.homepage.repository.clerk.survey;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import keeper.project.homepage.entity.clerk.survey.SurveyMemberReplyEntity;
+import keeper.project.homepage.entity.member.MemberEntity;
+import keeper.project.homepage.entity.clerk.survey.SurveyEntity;
+import keeper.project.homepage.entity.clerk.survey.SurveyReplyEntity;
+import keeper.project.homepage.entity.clerk.survey.SurveyReplyExcuseEntity;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SurveyRepositoryTest extends SurveyTestHelper{
+
+  @Test
+  @DisplayName("설문 조사의 응답자 수 확인")
+  public void getRespondentsInSurvey(){
+    //given
+    LocalDateTime openTime = LocalDateTime.of(2022,8,18,0,0,0);
+    LocalDateTime closeTime = LocalDateTime.of(2022,8,20,0,0,0);
+    SurveyEntity survey = generateSurvey(openTime,closeTime);
+    MemberEntity member = memberRepository.getById(1L);
+    SurveyReplyEntity reply = generateSurveyReply(SurveyReplyEntity.ACTIVITY);
+
+    generateSurveyMemberReply(survey,member,reply);
+
+    em.flush();
+    em.clear();
+
+    //when
+    SurveyEntity loadedSurvey = surveyRepository.getById(survey.getId());
+
+    //then
+    Assertions.assertThat(loadedSurvey.getRespondents().size()).isEqualTo(1);
+
+  }
+
+  @Test
+  @DisplayName("설문 조사의 응답이 휴면(기타)일 경우")
+  public void 휴면(){
+    //given
+    SurveyEntity survey = surveyRepository.getById(1L);
+    MemberEntity member = memberRepository.getById(1L);
+    SurveyReplyEntity reply = generateSurveyReply(SurveyReplyEntity.OTHER_DORMANT);
+
+    SurveyMemberReplyEntity surveyMemberReplyEntity = generateSurveyMemberReply(survey,member,reply);
+
+    SurveyReplyExcuseEntity surveyReplyExcuseEntity = generateSurveyReplyExcuse(surveyMemberReplyEntity,"BOB");
+
+    em.flush();
+    em.clear();
+
+    //when
+    SurveyMemberReplyEntity find = surveyMemberReplyRepository.getById(surveyMemberReplyEntity.getId());
+
+    //then
+    Assertions.assertThat(surveyReplyExcuseEntity.getRestExcuse()).isEqualTo(find.getSurveyReplyExcuseEntity().getRestExcuse());
+  }
+
+}
---
+++ src/test/java/keeper/project/homepage/repository/clerk/SurveyTestHelper.java
@@ -0,0 +1,92 @@
+package keeper.project.homepage.repository.clerk.survey;
+
+import java.time.LocalDateTime;
+import javax.persistence.EntityManager;
+import keeper.project.homepage.entity.member.MemberEntity;
+import keeper.project.homepage.entity.clerk.survey.SurveyEntity;
+import keeper.project.homepage.entity.clerk.survey.SurveyMemberReplyEntity;
+import keeper.project.homepage.entity.clerk.survey.SurveyReplyEntity;
+import keeper.project.homepage.entity.clerk.survey.SurveyReplyExcuseEntity;
+import keeper.project.homepage.repository.clerk.survey.SurveyMemberReplyRepository;
+import keeper.project.homepage.repository.clerk.survey.SurveyReplyExcuseRepository;
+import keeper.project.homepage.repository.clerk.survey.SurveyReplyRepository;
+import keeper.project.homepage.repository.clerk.survey.SurveyRepository;
+import keeper.project.homepage.repository.member.MemberRepository;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class SurveyTestHelper {
+
+  @Autowired
+  protected EntityManager em;
+
+  @Autowired
+  protected MemberRepository memberRepository;
+
+  @Autowired
+  protected SurveyMemberReplyRepository surveyMemberReplyRepository;
+  @Autowired
+  protected SurveyReplyExcuseRepository surveyReplyExcuseRepository;
+  @Autowired
+  protected SurveyReplyRepository surveyReplyRepository;
+  @Autowired
+  protected SurveyRepository surveyRepository;
+
+  protected SurveyEntity generateSurvey(LocalDateTime openTime,LocalDateTime closeTime) {
+    final long epochTime = System.nanoTime();
+    Boolean isVisible;
+    LocalDateTime now = LocalDateTime.now();
+    if (now.isAfter(openTime) && now.isBefore(closeTime)){
+      isVisible = true;
+    }
+    else{
+      isVisible = false;
+    }
+    return surveyRepository.save(
+        SurveyEntity.builder()
+            .name("name_" + epochTime)
+            .openTime(openTime)
+            .closeTime(closeTime)
+            .description("description_" + epochTime)
+            .isVisible(isVisible)
+            .build()
+    );
+  }
+
+  protected SurveyReplyEntity generateSurveyReply(SurveyReplyEntity reply){
+    return surveyReplyRepository.save(
+        SurveyReplyEntity.builder()
+            .type(reply.getType())
+            .build()
+    );
+  }
+
+  protected SurveyMemberReplyEntity generateSurveyMemberReply(SurveyEntity survey,MemberEntity member,
+      SurveyReplyEntity reply) {
+    return surveyMemberReplyRepository.save(
+        SurveyMemberReplyEntity.builder()
+            .member(member)
+            .survey(survey)
+            .reply(reply)
+            .replyTime(LocalDateTime.now())
+            .build()
+    );
+  }
+
+  protected SurveyReplyExcuseEntity generateSurveyReplyExcuse(SurveyMemberReplyEntity surveyMemberReplyEntity,String because){
+    return surveyReplyExcuseRepository.save(
+        SurveyReplyExcuseEntity.builder()
+            .surveyMemberReplyEntity(surveyMemberReplyEntity)
+            .restExcuse(because)
+            .build()
+    );
+
+  }
+}
