280a028111f016052a7200fd155fbc14b5d82829
JeonghakLee <wjdgkr0527@naver.com>
2022-08-19 11:52:14 +0900

    fix: 불필요한 메서드 및 연관된 테스트 제거

--- src/main/java/keeper/project/homepage/repository/clerk/SeminarRepository.java
+++ src/main/java/keeper/project/homepage/repository/clerk/SeminarRepository.java
@@ -1,10 +1,7 @@
 package keeper.project.homepage.repository.clerk;
 
 import keeper.project.homepage.entity.clerk.SeminarEntity;
-import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SeminarRepository extends JpaRepository<SeminarEntity, Long> {
-
-  SeminarEntity findByNameIsLike(@NonNull String name);
 }
