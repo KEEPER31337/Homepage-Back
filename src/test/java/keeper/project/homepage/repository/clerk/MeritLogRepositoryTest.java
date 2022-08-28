913a22005605154a2671a43cb949a76694026e1f
JeonghakLee <wjdgkr0527@naver.com>
2022-08-29 00:19:33 +0900

    test: 상벌점 Repository Test 작성

---
+++ src/test/java/keeper/project/homepage/repository/clerk/MeritLogRepositoryTest.java
@@ -0,0 +1,40 @@
+package keeper.project.homepage.repository.clerk;
+
+import java.time.LocalDateTime;
+import keeper.project.homepage.entity.clerk.MeritLogEntity;
+import keeper.project.homepage.entity.clerk.MeritTypeEntity;
+import keeper.project.homepage.entity.member.MemberEntity;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class MeritLogRepositoryTest extends MeritRepositoryTestHelper {
+
+  @Test
+  @DisplayName("[SUCCESS] 상벌점 내역 추가 테스트")
+  void addMeritLogTest() {
+    // given
+    MemberEntity awarder = generateMember();
+    MemberEntity giver = generateMember();
+    MeritTypeEntity type = meritTypeRepository.findById(1L).orElseThrow();
+    MeritLogEntity meritLog = MeritLogEntity.builder()
+        .awarder(awarder)
+        .giver(giver)
+        .meritType(type)
+        .time(LocalDateTime.now().withNano(0))
+        .build();
+
+    MeritLogEntity save = meritLogRepository.save(meritLog);
+    em.flush();
+    em.clear();
+
+    // when
+    MeritLogEntity find = meritLogRepository.findById(save.getId()).orElseThrow();
+
+    // then
+    Assertions.assertThat(find.getAwarder().getId()).isEqualTo(save.getAwarder().getId());
+    Assertions.assertThat(find.getGiver().getId()).isEqualTo(save.getGiver().getId());
+    Assertions.assertThat(find.getMeritType().getId()).isEqualTo(save.getMeritType().getId());
+    Assertions.assertThat(find.getTime()).isEqualTo(save.getTime());
+  }
+}
