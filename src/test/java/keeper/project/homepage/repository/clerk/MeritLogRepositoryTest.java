705b85d93ed04b68138bcf3a53e895fdee7e2da5
JeonghakLee <wjdgkr0527@naver.com>
2022-08-31 21:51:01 +0900

    test: 상벌점 페이지 API 성공 테스트 작성

--- src/test/java/keeper/project/homepage/repository/clerk/MeritLogRepositoryTest.java
+++ src/test/java/keeper/project/homepage/repository/clerk/MeritLogRepositoryTest.java
@@ -1,5 +1,6 @@
 package keeper.project.homepage.repository.clerk;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import keeper.project.homepage.entity.clerk.MeritLogEntity;
 import keeper.project.homepage.entity.clerk.MeritTypeEntity;
@@ -21,7 +22,7 @@ public class MeritLogRepositoryTest extends MeritRepositoryTestHelper {
         .awarder(awarder)
         .giver(giver)
         .meritType(type)
-        .time(LocalDateTime.now().withNano(0))
+        .time(LocalDate.now())
         .build();
 
     MeritLogEntity save = meritLogRepository.save(meritLog);
