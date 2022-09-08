ba50460e62bc84164fcf0a006fe31705511eeba3
shasuri <ghimmk@gmail.com>
2022-09-08 21:24:27 +0900

    fix : 불필요한 @Setter 제거
    Dto의 불필요한 `@Setter`를 제거했습니다.
    
    issue : #371
    pr : #401
         #discussion_r964711426

--- src/main/java/keeper/project/homepage/util/dto/ThumbnailFileIdDto.java
+++ src/main/java/keeper/project/homepage/util/dto/ThumbnailFileIdDto.java
@@ -5,10 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
