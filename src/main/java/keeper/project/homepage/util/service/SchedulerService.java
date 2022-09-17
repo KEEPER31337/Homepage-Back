6b3a033d022c8f8b9438f26920bb6647ab0515c0
woo-chang <clllickme@naver.com>
2022-09-17 21:35:26 +0900

    fix: Bean 등록을 통한 의존성 주입으로 변경
    issue: #407

--- src/main/java/keeper/project/homepage/util/service/SchedulerService.java
+++ src/main/java/keeper/project/homepage/util/service/SchedulerService.java
@@ -1,20 +1,17 @@
 package keeper.project.homepage.util.service;
 
 import java.util.Date;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.TaskScheduler;
-import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SchedulerService {
 
+  private final TaskScheduler taskScheduler;
+
   public void scheduleTask(Runnable task, Date date) {
-    ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-    TaskScheduler taskScheduler = new ConcurrentTaskScheduler(scheduledExecutorService);
     taskScheduler.schedule(task, date);
   }
 
