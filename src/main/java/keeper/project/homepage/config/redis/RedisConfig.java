aa0f2960f43f42bce9061780d22b50db515e72f8
정핸모 <gusah009@naver.com>
2022-10-12 10:21:44 +0900

    feat: 테스트시에만 Embedded Redis 사용을 위해 Configuration 수정
    issue: #443

--- src/main/java/keeper/project/homepage/config/redis/RedisConfig.java
+++ src/main/java/keeper/project/homepage/config/redis/RedisConfig.java
@@ -1,8 +1,11 @@
 package keeper.project.homepage.config.redis;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -16,9 +19,16 @@ public class RedisConfig {
   @Value("${spring.redis.port}")
   private int port;
 
+  public int getPort() {
+    if (port == -1) {
+      return port = findAvailablePort();
+    }
+    return port;
+  }
+
   @Bean
   public RedisConnectionFactory redisConnectionFactory() {
-    return new LettuceConnectionFactory(host, port);
+    return new LettuceConnectionFactory(host, getPort());
   }
 
   @Bean
@@ -26,5 +36,13 @@ public class RedisConfig {
     RedisTemplate<?, ?> redisTemplate = new RedisTemplate<>();
     redisTemplate.setConnectionFactory(redisConnectionFactory());
     return redisTemplate;
+  }
+
+  public int findAvailablePort() {
+    try (ServerSocket serverSocket = new ServerSocket(0)) {
+      return serverSocket.getLocalPort();
+    } catch (IOException e) {
+      throw new IllegalStateException("Port is not available");
+    }
   }
 }
