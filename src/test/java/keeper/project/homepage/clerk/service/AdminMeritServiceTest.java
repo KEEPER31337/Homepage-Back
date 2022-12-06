36d9567969edfb92831d8a0493ac74d6d62f213a
JeonghakLee <wjdgkr0527@naver.com>
2022-12-06 15:38:34 +0900

    test: 연도 리스트 조회 테스트 구현

--- src/test/java/keeper/project/homepage/clerk/service/AdminMeritServiceTest.java
+++ src/test/java/keeper/project/homepage/clerk/service/AdminMeritServiceTest.java
@@ -4,9 +4,15 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 import keeper.project.homepage.clerk.dto.request.MeritAddRequestDto;
+import keeper.project.homepage.clerk.entity.MeritLogEntity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,6 +31,24 @@ class AdminMeritServiceTest extends AdminClerkServiceTestHelper {
 
     // then
     assertThrows(IllegalArgumentException.class, () -> adminMeritService.deleteMeritType(1L));
+  }
+
+  @Test
+  @DisplayName("서로 다른 해에 상벌점 내역 추가시 연도 리스트에 추가되어야 한다")
+  void addMeritLogTest() {
+    // given
+    LocalDate toDay = LocalDate.now();
+    List<LocalDate> dates = List.of(toDay, toDay.minusYears(2L), toDay.plusYears(1L));
+    List<MeritAddRequestDto> requests = dates.stream()
+        .map(AdminMeritServiceTest::getVirtualMeritAddRequestDto).toList();
+
+    // when
+    requests.forEach(request -> adminMeritService.addMeritWithLog(request));
+    List<Integer> years = adminMeritService.getYears();
+
+    // then
+    assertThat(years).containsAll(
+        dates.stream().map(LocalDate::getYear).collect(Collectors.toList()));
   }
 
   private static MeritAddRequestDto getVirtualMeritAddRequestDto(LocalDate date) {
