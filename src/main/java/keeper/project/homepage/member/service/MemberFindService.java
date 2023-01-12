591a76c7295fdbfdf480563791e54f020a7cdb4b
woo-chang <clllickme@naver.com>
2023-01-12 11:59:40 +0900

    refactor: 조회 서비스는 readOnly 속성을 통한 성능 개선
    issue: #331

--- src/main/java/keeper/project/homepage/member/service/MemberFindService.java
+++ src/main/java/keeper/project/homepage/member/service/MemberFindService.java
@@ -13,8 +13,10 @@ import keeper.project.homepage.member.exception.CustomMemberNotFoundException;
 import keeper.project.homepage.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class MemberFindService {
 
